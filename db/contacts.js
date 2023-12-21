e1f6fe495b03af87eb47c0e82a951c4637e291e4
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-12-21 12:00:33 +0200

    task complied

--- db/contacts.js
+++ db/contacts.js
@@ -1,28 +1,85 @@
-// const fs = require("node:fs");
-// const fs = require("node:fs").promises;
+
 const fs = require("node:fs/promises");
+const path = require("node:path");
+const crypto = require("node:crypto");
 
-// fs.readFile("read.txt", { encoding: "utf-8" }, (error, data) => {
-//   if (error) {
-//     return console.error(error);
-//   }
+const contactsPath = path.join(__dirname, "..", "db", "contacts.json");
 
-//   console.log(data);
-// });
+async function readFile() {
+  const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
 
-// fs.promises
-//   .readFile("read.txt", { encoding: "utf-8" })
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error));
-
-// fs.readFile("read.txt", { encoding: "utf-8" })
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error));
-
-async function main() {
-  const data = await fs.readFile("contacts.json", { encoding: "utf-8" });
-
-  console.log(data);
+  return JSON.parse(data);
 }
 
-main().catch((error) => console.error(error));
+function writeFile(contacts) {
+  return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+}
+
+async function listContacts() {
+  const contacts = await readFile();
+
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await readFile();
+
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  return contact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await readFile();
+  const newContact = { id: crypto.randomUUID(), name, email, phone  };
+
+  contacts.push(newContact);
+
+  await writeFile(contacts);
+
+  return newContact;
+}
+
+// async function update(id, contact) {
+//   const contacts = await readFile();
+//   const index = contacts.findIndex((contact) => contact.id === id);
+
+//   if (index === -1) {
+//     return undefined;
+//   }
+
+//   const newContact = { ...contact, id };
+//   // const newBooks = [
+//   //   ...books.slice(0, index),
+//   //   newBook,
+//   //   ...books.slice(index + 1),
+//   // ];
+//   contacts[index] = newContact;
+
+//   await writeFile(contacts);
+
+//   return newBook;
+// }
+
+async function removeContact(contactId) {
+  const contacts = await readFile();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return undefined;
+  }
+
+  const newContacts = [...contacts.slice(0, index), ...contacts.slice(index + 1)];
+
+  await writeFile(newContacts);
+
+  return contacts[index];
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  // update,
+  removeContact,
+};
--- index.js
+++ index.js
@@ -1,2 +1,60 @@
-console.log(1234);
-console.log(123);
+const Contacts = require("./db/contacts");
+const { program } = require("commander");
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case "list":
+        const contacts = await  Contacts.listContacts();
+        return console.log(contacts);
+      case "get":
+        const contact = await  Contacts.getContactById(id);
+        return console.log(contact);
+      case "add":
+        const createdContact = await Contacts.addContact(name, email, phone);
+        return console.log("You created next new contact: ", createdContact);
+    
+      case "remove":
+        const removedContact = await Contacts.removeContact(id);
+        return console.log("This contact was removed: ", removedContact);
+      default:
+        console.log("Unknown action:(");
+    }
+  }
+  
+  program
+    .option("-action, --action <action>", "Action to invoke")
+    .option("-i, --id <id.toString>", "Contact id")
+    .option("-n, --name <name>", "Contact name")
+    .option("-e, --email <email>", "Contact email")
+    .option("-ph, --phone <phone>", "Contact phone number")
+  
+  program.parse(process.argv);
+  
+  const options = program.opts();
+  
+  invokeAction(options);
+
+
+// SHOW all contacts
+// contacts
+//   .listContacts()
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+// SHOW contact by ID
+//   contacts
+//   .getContactById('e6ywwRe4jcqxXfCZOj_1e')
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
+// SHOW deleted contact
+// contacts
+//   .removeContact('AeHIrLTr6JkxGE6SN-0Rw')
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
+//SHOW added contact
+// contacts
+//   .addContact('Alexander McQueen', 'alex@alexmcque.com', '(066) 123-9384')
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
